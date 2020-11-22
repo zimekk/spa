ac8a440fdf4a2bbd923c22f0f06a713178353281
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-22 20:27:55 +0000

    Deploy zimekk/spa to github.com/zimekk/spa.git:gh-pages

---
+++ precache-manifest.90cec3a9eacd9f55787bf09fd4ca29a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997b720de3cb5950868aecf86f530ece",
+    "url": "/spa/index.html"
+  },
+  {
+    "revision": "cdd41b4e3cc6be66ca70",
+    "url": "/spa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "492d72670175c576f482",
+    "url": "/spa/static/js/2.dc6b89e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/spa/static/js/2.dc6b89e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd41b4e3cc6be66ca70",
+    "url": "/spa/static/js/main.3a2f96c1.chunk.js"
+  },
+  {
+    "revision": "4a909af6cb52d85eb879",
+    "url": "/spa/static/js/runtime-main.0da57ed0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spa/static/media/logo.5d5d9eef.svg"
+  }
+]);
